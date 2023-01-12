a8cba16a853cb80b22b66532951ae93220b9b05e
petricaMilea <petrica.v.milea@gmail.com>
2023-01-12 21:40:58 +0200

    create agency

---
+++ Models/Agency.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Proiect.Models
+{
+    public class Agency
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Contact { get; set; }
+    }
+}
