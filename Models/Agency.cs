b903de02dcedad5f15f3fd5275c41aecf2e2cb73
petricaMilea <petrica.v.milea@gmail.com>
2023-01-12 21:42:30 +0200

    modificare

--- Models/Agency.cs
+++ Models/Agency.cs
@@ -6,6 +6,7 @@ namespace Proiect.Models
     public class Agency
     {
         public int ID { get; set; }
+        [Display(Name ="Travel Agency")]
         public string Name { get; set; }
         public string Address { get; set; }
         public string Contact { get; set; }
